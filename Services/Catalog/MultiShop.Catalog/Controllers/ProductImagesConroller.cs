c57f58acfa9642929f1ce142c0c228ab997399c7
huseyinsamsunn <huseyinsamsun81@gmail.com>
2024-05-05 01:39:03 +0300

    add controller and program cs add lifecycle

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesConroller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    public class ProductImagesConroller : ControllerBase
+    {
+        private readonly IProductImageService _productImageService;
+        public ProductImagesConroller(IProductImageService productImageService)
+        {
+            _productImageService = productImageService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _productImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = _productImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _productImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün Fotoğrafı Başarı Ile Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _productImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün Fotoğrafı Başarı İle Silindi");
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _productImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün Fotoğrafı Başarı İle Güncellendi");
+
+        }
+    }
+}
